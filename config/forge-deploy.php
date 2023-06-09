d5c0da0a24590e00d56eb2144359bb8780d931a8
Galen Gidman <galen@galengidman.com>
2023-06-08 22:47:04 -0500

    add example environment config

--- config/forge-deploy.php
+++ config/forge-deploy.php
@@ -4,6 +4,14 @@ return [
 
     'per_page' => env('FORGE_DEPLOY_PER_PAGE', 25),
 
-    'environments' => [],
+    'environments' => [
+
+        // 'staging' => [
+        //     'server' => env('FORGE_DEPLOY_STAGING_SERVER'),
+        //     'site' => env('FORGE_DEPLOY_STAGING_SITE'),
+        //     'token' => env('FORGE_DEPLOY_STAGING_TOKEN'),
+        // ],
+
+    ],
 
 ];
